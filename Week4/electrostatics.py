7da5c62768caa807701b7004b92f4f857cf30f71
Josh Ziegler <jeziegle@csm-pm-38123-16.local>
2013-04-25 15:58:12 -0700

    added notes and classwork

---
+++ Week4/electrostatics.py
@@ -0,0 +1,10 @@
+
+def pointPotential(x , y, q, posx, posy):
+    """Finds potential at x and y due to a point charge of charge q at posx, and posy"""
+    k = 8.987551787368176*10**9
+    return (k*q/((x-posx)**2+(y-posy)**2)**.5)
+
+def dipolePotential(x, y, q, d):
+    """Finds potential at x and y due to a dipole a distance d separated"""
+    v = pointPotential(x, y, q, -d*.5, 0) + pointPotential(x, y, -q, d*.5, 0)
+    return v
