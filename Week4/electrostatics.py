4e8638c9f97461fe7c20c0fb699daba7a4dd250a
Josh Ziegler <jeziegle@csm-pm-38123-03.local>
2013-05-07 15:47:26 -0700

    Midterm file and rellated sd stuff added

--- Week4/electrostatics.py
+++ Week4/electrostatics.py
@@ -8,3 +8,10 @@ def dipolePotential(x, y, q, d):
     """Finds potential at x and y due to a dipole a distance d separated"""
     v = pointPotential(x, y, q, 0, -d*.5) + pointPotential(x, y, -q, 0, d*.5)
     return v
+
+def pointField(x,y,q,Xq,Yq):
+    """Takes arrays (x,y) charge q and position Xq, Yq and returns a tuple of the electric field components (Ex,Ey)"""
+    k = 8.987551787368176*10.**9.
+    Ex = k*q*(x-Xq)/((x-Xq)**2+(y-Yq)**2)**.5
+    Ey = k*q*(y-Yq)/((x-Xq)**2+(y-Yq)**2)**.5
+    return Ex,Ey
